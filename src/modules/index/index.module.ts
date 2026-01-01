f8eff6c5fa91366cdfbe5009fd10c9d593960972
dvora-wa <dw4121060@gmail.com>
2026-01-01 21:55:02 +0200

    Fix import path for GptService in index.module.ts

--- src/modules/index/index.module.ts
+++ src/modules/index/index.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { IndexController } from './index.controller';
 import { IndexService } from './index.service';
 import { PdfModule } from '../pdf/pdf.module';
-import { GptService } from 'src/modules/index/gpt.service';
+import { GptService } from './gpt.service';
 
 @Module({
   imports: [PdfModule],
