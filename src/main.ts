d969aa60bb458c4737f3067f85f358aeb741071f
dvora-wa <dw4121060@gmail.com>
2026-01-01 15:33:20 +0200

    Small changes

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-
-// @Catch()
-// export class HttpExceptionFilter implements ExceptionFilter {
-//   catch(exception: unknown, host: ArgumentsHost) {
-//     const ctx = host.switchToHttp();
-//     const response = ctx.getResponse();
-
-//     if (exception instanceof HttpException) {
-//       const status = exception.getStatus();
-//       const res = exception.getResponse();
-
-//       return response.status(status).json({
-//         error: res,
-//       });
-//     }
-
-//     return response.status(500).json({
-//       error: exception instanceof Error ? exception.message : 'Unknown error',
-//     });
-//   }
-// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // app.useGlobalFilters(new HttpExceptionFilter());
 
-    // Enable CORS for Angular frontend
+  // Enable CORS for Angular frontend
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL ?? true
+    //  || 'http://localhost:4200'
+    ,
     credentials: true,
   });
 
@@ -43,7 +23,7 @@ async function bootstrap() {
   }));
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server is running on port ${port}`);
 }
 bootstrap();
